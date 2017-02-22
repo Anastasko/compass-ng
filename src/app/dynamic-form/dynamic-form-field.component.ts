be17dd255dfe5f9f308ca0f604b376677c2f2494
volodja <volodja.extrim@gmail.com>
2017-02-22 02:26:37 +0200

    select entity

--- src/app/dynamic-form/dynamic-form-field.component.ts
+++ src/app/dynamic-form/dynamic-form-field.component.ts
@@ -1,19 +1,38 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormGroup}        from '@angular/forms';
 import {FieldViewModel} from "../common/model/field-view-model";
+import {ServiceFactory} from "../api/service-factory.service";
+import {Service} from "../common/service.service";
 
 @Component({
-    selector: 'df-field',
-    templateUrl: 'dynamic-form-field.component.html'
+  selector: 'df-field',
+  templateUrl: 'dynamic-form-field.component.html'
 })
-export class DynamicFormFieldComponent {
+export class DynamicFormFieldComponent implements OnInit {
 
-    @Input() field: FieldViewModel;
-    @Input() form: FormGroup;
+  private entities : any[] = [];
 
-    get isValid() {
-        return this.form.controls[this.field.fieldName].valid;
+  @Input() field: FieldViewModel;
+  @Input() form: FormGroup;
+
+  ngOnInit(): void {
+    if (this.field.fieldType.typeKind == 'ENTITY') {
+      this.getFieldService().findAll().then(data => {
+        console.log(data);
+        this.entities = data;
+      })
     }
+  }
 
+  constructor(private serviceFactory: ServiceFactory) {
+  }
+
+  get isValid() {
+    return this.form.controls[this.field.fieldName].valid;
+  }
+
+  getFieldService(): Service<any> {
+    return this.serviceFactory.getService(this.field.fieldType.id);
+  }
 
 }
