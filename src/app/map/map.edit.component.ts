dbceafe0b24e191b771afe3b6741f3bf47fe943f
volodja <volodja.extrim@gmail.com>
2017-03-07 00:16:26 +0200

    + to d3

--- src/app/map/map.edit.component.ts
+++ src/app/map/map.edit.component.ts
@@ -1,18 +1,28 @@
-import {Component, Input, OnInit, OnDestroy} from '@angular/core';
+import {Component, Input, OnInit, OnDestroy, NgZone} from '@angular/core';
 import {MapService} from '../api/service/map.service';
 import {EntityMap} from  '../api/model/entity-map';
 import {Routes, ActivatedRoute} from '@angular/router';
 import {Http, Response} from '@angular/http';
 import * as d3 from 'd3';
 import {config} from "../config";
+import {Entity} from "../common/model/entity";
 
 @Component({
     selector: 'map',
     template: `Map: #
         {{map?.id}}  {{status}}
         <div id="map" style="height: calc(100vh - 110px); border: 1px solid black"></div>
+        <div id="map_item_menu" [style.display]="hideMenu ? 'none' : 'block'">
+          123
+        </div>
+        <div class="tooltip" style="display: none">
+        tooltip
+        </div>
       `,
-    providers: [MapService]
+    providers: [MapService],
+    styleUrls: [
+      'map-item-tooltip.css'
+    ]
 })
 export class MapEditComponent implements OnInit, OnDestroy {
 
@@ -21,25 +31,38 @@ export class MapEditComponent implements OnInit, OnDestroy {
 
     status: string = "Loading ...";
 
+    private hideMenu: boolean = true;
+    private dragging: boolean = false;
+    private tooltip: any;
     private sub: any;
     private mapItemRadius = 8;
     private mapItems: any[];
     private svg: any;
+    private owner: Entity;
+    private transform: any = {
+      x: 0,
+      y: 0,
+      k: 1
+    };
 
     constructor(private _http: Http,
                 private _mapService: MapService,
-                private route: ActivatedRoute) {
+                private route: ActivatedRoute,
+                private ngZone: NgZone) {
 
     }
 
     ngOnInit() {
+        this.tooltip = d3.select(".tooltip");
         this.sub = this.route.params.subscribe(params => {
-            let p1 = this._mapService.findOne(params['id'])
+            this.owner = new Entity({
+              id: +params['id']
+            });
+            this._mapService.findOne(this.owner.id)
                 .then(map => {
                     this.map = map;
                     this.initMap();
                 });
-
         });
     }
 
@@ -55,6 +78,7 @@ export class MapEditComponent implements OnInit, OnDestroy {
             .attr("height", "100%")
             .call(d3.zoom().on("zoom", function () {
                 let transform = d3.event.transform;
+                that.transform = transform;
                 that.svg.attr("transform", 'translate(' + transform.x + ',' + transform.y + ') scale(' + transform.k + ')');
                 d3.selectAll('.map-item').attr('r', that.mapItemRadius / transform.k);
             }))
@@ -73,16 +97,38 @@ export class MapEditComponent implements OnInit, OnDestroy {
                 that.initMapItems();
                 that.status = "OK";
             });
+        d3.select('#map').on('click', () => {
+          that.hideMenu = true;
+          that.tooltip.style("display", "none");
+        });
     }
 
     initMapItems() {
-        this.mapItems=[];
+        this.mapItems=[{
+          id: 44,
+          x: 100,
+          y: 400
+        }];
         let that = this;
         this.svg.selectAll(".map-item")
             .data(this.mapItems)
             .enter().append("circle")
-            .on("click", function (i : any) {
-                // that.currentItem = i;
+            .on("click", v => {
+                that.tooltip
+                  .text(v.id)
+                  .style("left", (that.transform.x + v.x * that.transform.k - 30) + "px")
+                  .style("top", (70 + that.transform.y + v.y * that.transform.k - 12) + "px")
+                  .style("display", "block");
+                d3.event.stopPropagation();
+            })
+            .on("contextmenu", function() {
+              let position = d3.mouse(this);
+              that.hideMenu = false;
+              d3.select('#map_item_menu')
+                .style('position', 'absolute')
+                .style('left', (that.transform.x + position[0] * that.transform.k) + "px")
+                .style('top', (85 + that.transform.y + position[1] * that.transform.k) + "px");
+              d3.event.preventDefault();
             })
             .attr("class", "map-item")
             .attr("cx", function (d: any) {
@@ -96,14 +142,28 @@ export class MapEditComponent implements OnInit, OnDestroy {
             .style("cursor", "move")
             .call(d3.drag()
                 .on("start", function () {
-                    d3.select(this).raise().classed("active", true);
+                    that.dragging = true;
+                    that.tooltip.style("display", "none");
+                    d3.select(this).raise().style("fill", 'green');
+                    that.hideMenu = true;
                 })
                 .on("drag", function (d: any) {
                     d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
                 })
                 .on("end", function () {
-                    d3.select(this).classed("active", false);
-                }));
+                    d3.select(this).style("fill", 'orange');
+                    that.dragging = false;
+                }))
+            .on("mouseover", function() {
+              if (!that.dragging){
+                  d3.select(this).style("fill", 'orangered');
+              }
+            })
+            .on("mouseout", function() {
+              if (!that.dragging){
+                d3.select(this).style("fill", "orange");
+              }
+            });
     }
 
 
